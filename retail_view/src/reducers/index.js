ceedbd3e39d832089fd161e4142f5311529fb55a
Satvinder <panesar.satvindersingh@gmail.com>
2018-11-05 11:56:51 -0500

    moving retail_view to Redux directory

--- retail_view/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import { retail_data } from './retailData.reducer';
-
-const myReducers = combineReducers({
-    retail_data
-});
-
-export default myReducers;
--- retail_view/src/sagas/index.js
+++
@@ -1,8 +0,0 @@
-import { all } from "redux-saga/effects";
-import { getRetailData } from './getData.saga';
-
-export default function* mySaga() {
-  yield all([
-    ...getRetailData
-  ]);
-}
