cdf44f1d9fead2a2a44905e56da81ca8b17fbd4c
Satvinder <panesar.satvindersingh@gmail.com>
2018-11-21 16:32:43 -0500

    data refresh and update each time api is accessed

--- BitoApp/backend/app.py
+++ BitoApp/backend/app.py
@@ -5,12 +5,29 @@ app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/bito_db"
 mongo = PyMongo(app)
 
-@app.route("/")
+@app.route("/getTable1Data")
 def table1_data():
 	sales_data = mongo.db.table1.find()
 	output = []
 	for ele in sales_data:
-		output.append({'retail_sales' : ele['retail_sales'], 'whole_sales' : ele['whole_sales'], 'units_sold': ele['units_sold']})
+
+		# get existing details
+		record_id = ele['record_id']
+		retail_sales = ele['retail_sales']
+		whole_sales = ele['whole_sales']
+		units_sold = ele['units_sold']
+
+		# update details
+		retail_sales += 1
+		whole_sales += 1
+		units_sold += 1
+
+		# update records in db
+		mongo.db.table1.update({'record_id': record_id}, {'record_id' : record_id, 'retail_sales': retail_sales, 'whole_sales': whole_sales, 'units_sold': units_sold})
+
+		# prepare output
+		output.append({'record_id' : record_id, 'retail_sales' : retail_sales, 'whole_sales' : whole_sales, 'units_sold': units_sold})
+	
 	return jsonify({'result' : output})
 
 if __name__ == '__main__':
