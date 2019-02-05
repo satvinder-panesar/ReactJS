28359347cdf144e7169122b2929f4e772c8447e9
Satvinder <panesar.satvindersingh@gmail.com>
2019-02-05 16:58:30 -0500

    authentication completed

---
+++ unsplash_api/src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+const CLIENT_ID = "415e38dc4d9a9dbf451ab8eb12217bd86fe03dec5630f1eba503f633275b35c9";
+const CLIENT_SECRET = "1b50c62379dec99cad11b3b10fae1fb8bdeb9dcb5d4725ce324c1cd9f419557e"
+class App extends Component {
+
+  constructor(){
+    super()
+    this.state = {authorized: false, access_token: null}
+  }
+
+  async componentDidMount() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const code = urlParams.get('code');
+    await fetch('https://unsplash.com/oauth/token',{
+          method: "post",
+          headers: {
+             "Content-Type": "application/x-www-form-urlencoded"
+          },
+          body: "client_id="+CLIENT_ID+"&client_secret="+CLIENT_SECRET+"&redirect_uri=http://localhost:3000/&code="+code+"&grant_type=authorization_code"
+        })
+        .then((response) => response.json())
+        .then((json) => this.setState({authorized: true, access_token: json['access_token']}))
+    console.log(this.state.access_token)
+  }
+
+  render() {
+    return (
+      <div>
+        <a
+            href={`https://unsplash.com/oauth/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=http://localhost:3000/`}
+        >Login</a>
+      </div>
+    );
+  }
+}
+
+export default App;
