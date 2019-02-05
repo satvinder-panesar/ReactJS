83aa8781e16eb166e2927234c15a3a2a7460bef7
Satvinder <panesar.satvindersingh@gmail.com>
2019-02-05 17:09:41 -0500

    screen change on authentication

--- unsplash_api/src/App.js
+++ unsplash_api/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Welcome from './components/Welcome';
 
 const CLIENT_ID = "415e38dc4d9a9dbf451ab8eb12217bd86fe03dec5630f1eba503f633275b35c9";
 const CLIENT_SECRET = "1b50c62379dec99cad11b3b10fae1fb8bdeb9dcb5d4725ce324c1cd9f419557e"
@@ -6,7 +7,7 @@ class App extends Component {
 
   constructor(){
     super()
-    this.state = {authorized: false, access_token: null}
+    this.state = {authorized: null, access_token: null}
   }
 
   async componentDidMount() {
@@ -27,10 +28,12 @@ class App extends Component {
   render() {
     return (
       <div>
-        <a
-            href={`https://unsplash.com/oauth/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=http://localhost:3000/`}
-        >Login</a>
-      </div>
+        {!this.state.access_token && <center>
+          <a
+              href={`https://unsplash.com/oauth/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=http://localhost:3000/`}
+          >Authorize</a></center>}
+        {this.state.access_token && <Welcome />}
+       </div>
     );
   }
 }
---
+++ unsplash_api/src/components/Welcome.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Welcome extends Component {
+
+  constructor(){
+    super()
+  }
+
+  render() {
+    return (
+      <div>Welcome</div>
+    );
+  }
+}
+
+export default Welcome;
