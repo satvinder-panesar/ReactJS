0e01371c716582e207bf6aa1fd140ac193ceed69
Satvinder <panesar.satvindersingh@gmail.com>
2019-02-05 18:28:38 -0500

    fetching users done

--- unsplash_api/src/components/Welcome.js
+++ unsplash_api/src/components/Welcome.js
@@ -8,7 +8,7 @@ class Welcome extends Component {
     this.handleSearch = this.handleSearch.bind(this)
     this.handleChange = this.handleChange.bind(this)
     this.selectUser = this.selectUser.bind(this)
-    this.state = {user: null, selected_user: null, users: []}
+    this.state = {user: null, selected_user: null, users: null}
   }
 
   selectUser(user){
@@ -19,8 +19,24 @@ class Welcome extends Component {
     this.setState({user: event.target.value})
   }
 
-  handleSearch(){
-    this.setState({users: ["a","b","c","d"]})
+  async handleSearch(){
+    if(this.state.user == null){
+      alert("Enter Username")
+    }else{
+      let temp = []
+      await fetch('https://api.unsplash.com/search/users?query='+this.state.user,{
+            method: "get",
+            headers: {
+               Authorization: 'Bearer '+this.props.access_token
+            }})
+          .then((response) => response.json())
+          .then((json) => {
+            for(let i in json['results']){
+              temp.push(json['results'][i]['name'])
+            }
+          })
+      this.setState({users: temp})
+  }
   }
 
   render() {
